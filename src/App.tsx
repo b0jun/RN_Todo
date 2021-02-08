261ef9b5f1a36ee06be7be5c003d892adaf90b52
billowycloud <bjkim0228@naver.com>
2021-02-08 14:40:29 +0900

    Init Setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default App;
