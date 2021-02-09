37e8188b8a3bbd8c32af8ac2e28feb578158c97f
billowycloud <bjkim0228@naver.com>
2021-02-09 23:26:35 +0900

    Add Todo Feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import {TodoListContextProvider} from './Context/TodoListContext';
+import Todo from './Screens/Todo';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #eee;
+`;
 
 interface Props {}
 
 const App = ({}: Props) => {
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <TodoListContextProvider>
+      <Container>
+        <Todo />
+      </Container>
+    </TodoListContextProvider>
   );
 };
 
